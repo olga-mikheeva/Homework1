02c97e2df615db726725088bbb49cf2a6ad0fe64
Olga <workpost.mkh@yandex.ru>
2023-08-06 23:25:47 +0300

    Task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// 9 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+
+while(count <= number)
+{
+    Console.Write(count); 
+    count = count + 2;  
+    if (count <= number)
+    {
+        Console.Write(", ");
+    }
+}
+
